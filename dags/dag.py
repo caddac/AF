fc3f8c8a5b93fbca4ff5b461e97bf43ad190b61b
Dave Cavaletto <dave@fitv.io>
2020-05-02 16:57:57 -0600

    start date

--- dags/dag.py
+++ dags/dag.py
@@ -1,9 +1,12 @@
+from datetime import datetime
+
 from airflow import DAG
 from airflow.operators.http_operator import SimpleHttpOperator
 
-with DAG('firstDag', 'runs a dag', '0 0 * * *'):
+with DAG('firstDag', 'runs a dag', '0 0 * * *', start_date=datetime(2019, 1, 1), ):
   get_time = SimpleHttpOperator(
       task_id="get_time_to_xcom",
+
       http_conn_id='world_time',
       endpoint='/api/timezone/America/Boise',
       method='GET',
