86930d0fa11fc197623fb6cce21e508b60b5214f
Dave Cavaletto <dave@fitv.io>
2020-05-02 16:43:24 -0600

    put code back

---
+++ dashboard.sh
@@ -0,0 +1,12 @@
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.0.0/aio/deploy/recommended.yaml
+
+kubectl apply -f dashboard/dashboard-adminuser.yml
+kubectl apply -f dashboard/admin-cluster-role-binding.yaml
+
+kubectl proxy &
+echo "proxy running as pid ${!}"
+
+kubectl -n kubernetes-dashboard describe secret $(kubectl -n kubernetes-dashboard get secret | grep admin-user | awk '{print $1}')
+
+
+echo 'http://localhost:8001/api/v1/namespaces/kubernetes-dashboard/services/https:kubernetes-dashboard:/proxy/'
